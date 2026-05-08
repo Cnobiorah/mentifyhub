634bd57be6958735655a565137b2e3aa2773b75b
Cnobiorah <156436795+Cnobiorah@users.noreply.github.com>
2026-05-08 05:32:28 -0700

    Delete api directory

--- api/ping.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json({ status: "warm", timestamp: new Date().toISOString() });
-};
